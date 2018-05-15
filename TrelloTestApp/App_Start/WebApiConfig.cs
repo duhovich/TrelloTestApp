f9ba8d339b36fb938a092608b70fae9c8447b66a
duhovich <olegduhovichrus@gmail.com>
2018-05-15 14:25:54 +0300

    Added initial processing

--- TrelloTestApp/App_Start/WebApiConfig.cs
+++ TrelloTestApp/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace TrelloTestApp
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Load receivers
+            config.InitializeReceiveTrelloWebHooks();
         }
     }
 }
---
+++ TrelloTestApp/Controllers/TrelloWebhookHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.WebHooks;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TrelloTestApp.Controllers
+{
+    public class TrelloWebHookHandler : WebHookHandler
+    {
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            Trace.TraceInformation(DateTime.Now.ToShortTimeString() + Environment.NewLine);
+            Trace.TraceInformation(Receiver + Environment.NewLine);
+            Trace.TraceInformation(context.Actions.First() + Environment.NewLine);
+            Trace.TraceInformation(context.Data + Environment.NewLine);
+
+            return Task.FromResult(true);
+        }
+    }
+}
